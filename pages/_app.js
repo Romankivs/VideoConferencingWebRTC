1126578a3f000e43d9f2bbd4de987976501ddbc9
Sviatoslav Romankiv <romankiv1771@gmail.com>
2023-04-16 18:18:02 +0300

    Remove margin body

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import App from '.'
-import "bootstrap/dist/css/bootstrap.min.css";
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
