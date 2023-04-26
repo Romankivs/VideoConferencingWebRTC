fee7bfb8fa5dc9ac5447788f3db5e9860b0bebad
Sviatoslav Romankiv <romankiv1771@gmail.com>
2023-04-26 19:58:06 +0300

    added ui to display username in a video

--- components/VideoGrid.jsx
+++ components/VideoGrid.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react";
 import styles from '../css_modules/VideoGrid.module.css'
 
-function Video({stream, id, muted}) {
+function Video({stream, id, username, muted}) {
     const videoRef = useRef();
   
     useEffect(() => {
@@ -9,17 +9,20 @@ function Video({stream, id, muted}) {
     }, []);
   
     return (
-      <div className="col h-100 m-2 border border-primary bg-secondary rounded">
+      <div className={styles.videoMask + " col h-100 m-2 border border-primary bg-secondary rounded"}>
         <video className={styles.video} ref = {videoRef} id = {id}
         muted = {muted} autoPlay = {true}>
         </video>
+        <div className={styles.usernameContainer}>
+          <h5 className = {styles.username + " font-weight-normal text-white"}>{username}</h5>
+        </div>
       </div>
     );
 }
 
 export default function VideoGrid({videos}) {
   const listVideos = videos.map((video, i) =>
-    <Video key = {video.id} stream = {video.stream} id = {video.id} muted = {video.muted}></Video>
+    <Video key = {video.id} stream = {video.stream} id = {video.id} username = {video.username} muted = {video.muted}></Video>
   );  
 
   return (<div className="container-fluid  p-5" id="videoGrid"><div className="row h-100">{listVideos}</div></div>)
